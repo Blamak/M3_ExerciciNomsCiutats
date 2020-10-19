7f7aef8503b4cdb2f93bc8408e74eaf98796db16
Blamak <vlasherassanchez@gmail.com>
2020-10-19 20:21:41 +0200

    Java Fase1 ok

--- Java/src/Fase1App.java
+++ Java/src/Fase1App.java
@@ -1,4 +1,33 @@
+import java.util.Scanner;
 
 public class Fase1App {
-
+	public static void main(String[] args) {
+		
+		String ciutat1;
+		String ciutat2;
+		String ciutat3;
+		String ciutat4;
+		String ciutat5;
+		String ciutat6;
+		
+		Scanner ciutats = new Scanner(System.in);
+		System.out.println("Introdueix sis ciutats: ");
+		
+		ciutat1 = ciutats.nextLine();
+		ciutat2 = ciutats.nextLine();
+		ciutat3 = ciutats.nextLine();
+		ciutat4 = ciutats.nextLine();
+		ciutat5 = ciutats.nextLine();
+		ciutat6 = ciutats.next();
+		
+		ciutats.close();
+		
+		System.out.println(ciutat1);
+		System.out.println(ciutat2);
+		System.out.println(ciutat3);
+		System.out.println(ciutat4);
+		System.out.println(ciutat5);
+		System.out.println(ciutat6);
+	}
 }
+	
